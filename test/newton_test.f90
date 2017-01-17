c94c9d86a2068504f4d4fe2bd93445e6f57ae3d6
Carlos Planelles <planelles20@gmail.com>
2017-01-17 17:49:35 +0100

    file to test newton raphson metrhod 1D

---
+++ test/newton_test.f90
@@ -0,0 +1,21 @@
+!-----------------------------------------------------------------------
+! Program to test Newton-Raphson Method
+!
+! Licensing: This code is distributed under the GNU GPL license.
+! Author: Carlos Planelles Alemany, planelles20(at)gmail(dot)com
+!-----------------------------------------------------------------------
+
+program newton_test
+use module_newton_raphson
+use whatever_function
+implicit none
+
+real(real64) :: x0 = 0.0, eps = 1e-5
+logical :: ok
+integer :: ite = 100
+
+call newton_raphson1D(x0, ite, eps, ok)
+
+print *, x0
+print *, ite 
+end program
