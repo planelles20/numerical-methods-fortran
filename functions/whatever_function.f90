cab3e95cc72fbac874a126f3debc9d9c01afd19f
Carlos Planelles <planelles20@gmail.com>
2017-01-18 02:07:55 +0100

    add new function

--- functions/whatever_function.f90
+++ functions/whatever_function.f90
@@ -31,11 +31,11 @@ contains
     ! f_3D: x --> R**3    x in R**3
     function funcion3D(x)
         real(real64), dimension(3) :: funcion3D
-        real(real64), intent(in) :: x(3)
+        real(real64), intent(in) :: x(:)
 
-        funcion3D(1) = exp(x(1)-x(2))+x(3)
-        funcion3D(2) = x(1)-x(2)+x(3)
-        funcion3D(3) = x(1)*x(3)-x(2)
+        funcion3D(1) = exp(x(1)) + x(2) - 1._real64
+        funcion3D(2) = 2.0*x(1)+x(2)+x(3) + 2._real64
+        funcion3D(3) = x(1)+x(2)+3.0*x(3) - 3._real64
     end function
 
     !Jacobian matrix of f_3D
@@ -43,17 +43,17 @@ contains
         real(real64), dimension(3,3) :: J_funcion3D
         real(real64), intent(in) :: x(3)
 
-        J_funcion3D(1,1) =  exp(x(1)-x(2))
-        J_funcion3D(1,2) = -exp(x(1)-x(2))
-        J_funcion3D(1,3) =  1.0
+        J_funcion3D(1,1) = exp(x(1))
+        J_funcion3D(1,2) = 1._real64
+        J_funcion3D(1,3) = 0._real64
 
-        J_funcion3D(2,1) =  1.0
-        J_funcion3D(2,2) = -1.0
-        J_funcion3D(2,3) =  1.0
+        J_funcion3D(2,1) = 2._real64
+        J_funcion3D(2,2) = 1._real64
+        J_funcion3D(2,3) = 1._real64
 
-        J_funcion3D(3,1) =  x(3)
-        J_funcion3D(3,2) = -1.0
-        J_funcion3D(3,3) =  x(1)
+        J_funcion3D(3,1) = 1._real64
+        J_funcion3D(3,2) = 1._real64
+        J_funcion3D(3,3) = 3._real64
     end function
 
 end module
